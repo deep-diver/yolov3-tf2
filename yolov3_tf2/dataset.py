80a60857f3bbe2e132ec67623580c159153348bc
chansung park <deep.diver.csp@gmail.com>
2019-04-25 00:10:34 +0900

    .

--- yolov3_tf2/dataset.py
+++ yolov3_tf2/dataset.py
@@ -102,9 +102,10 @@ def parse_tfrecord(tfrecord, class_table):
     x_train = tf.image.decode_jpeg(x['image/encoded'], channels=3)
     x_train = tf.image.resize(x_train, (416, 416))
 
-    class_text = tf.sparse.to_dense(
-        x['image/object/class/text'], default_value='')
-    labels = tf.cast(class_table.lookup(class_text), tf.float32)
+    # class_text = tf.sparse.to_dense(
+    #     x['image/object/class/text'], default_value='')
+    # labels = tf.cast(class_table.lookup(class_text), tf.float32)
+    labels = x['image/object/class/label']
     y_train = tf.stack([tf.sparse.to_dense(x['image/object/bbox/xmin']),
                         tf.sparse.to_dense(x['image/object/bbox/ymin']),
                         tf.sparse.to_dense(x['image/object/bbox/xmax']),
