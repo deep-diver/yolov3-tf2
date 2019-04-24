5c3ec1e7f4245d513d17057ef897b921c221aeaf
chansung park <deep.diver.csp@gmail.com>
2019-04-25 00:53:55 +0900

    .

--- yolov3_tf2/dataset.py
+++ yolov3_tf2/dataset.py
@@ -90,7 +90,7 @@ IMAGE_FEATURE_MAP = {
     'image/object/bbox/xmax': tf.io.VarLenFeature(tf.float32),
     'image/object/bbox/ymax': tf.io.VarLenFeature(tf.float32),
     'image/object/class/text': tf.io.VarLenFeature(tf.string),
-    'image/object/class/label': tf.io.VarLenFeature(tf.float32),
+    'image/object/class/label': tf.io.VarLenFeature(tf.int64),
     # 'image/object/difficult': tf.io.VarLenFeature(tf.int64),
     # 'image/object/truncated': tf.io.VarLenFeature(tf.int64),
     # 'image/object/view': tf.io.VarLenFeature(tf.string),
@@ -102,14 +102,14 @@ def parse_tfrecord(tfrecord, class_table):
     x_train = tf.image.decode_jpeg(x['image/encoded'], channels=3)
     x_train = tf.image.resize(x_train, (416, 416))
 
-    # class_text = tf.sparse.to_dense(
-    #     x['image/object/class/text'], default_value='')
-    # labels = tf.cast(class_table.lookup(class_text), tf.float32)
+    class_text = tf.sparse.to_dense(
+        x['image/object/class/text'], default_value='')
+    labels = tf.cast(class_table.lookup(class_text), tf.float32)
     y_train = tf.stack([tf.sparse.to_dense(x['image/object/bbox/xmin']),
                         tf.sparse.to_dense(x['image/object/bbox/ymin']),
                         tf.sparse.to_dense(x['image/object/bbox/xmax']),
                         tf.sparse.to_dense(x['image/object/bbox/ymax']),
-                        tf.sparse.to_dense(x['image/object/class/label'])], axis=1)
+                        labels], axis=1)
 
     paddings = [[0, 100 - tf.shape(y_train)[0]], [0, 0]]
     y_train = tf.pad(y_train, paddings)
